6155dd2df23c122251e1235aed383f49fa70021d
MaiKhoa0101 <maikhoa2015@gmail.com>
2025-06-17 18:13:54 +0700

    change little bit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -150,6 +150,7 @@ dependencies {
     //Tải và hiển thị hình ảnh online
     implementation(platform("androidx.compose:compose-bom:2025.05.00"))
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
 
 
     implementation("androidx.compose.material:material:1.8.1")
@@ -157,7 +158,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("com.google.accompanist:accompanist-flowlayout:0.36.0")
 
-    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
 
     val nav_version = "2.9.0"
 
